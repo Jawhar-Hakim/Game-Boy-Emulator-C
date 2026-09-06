26222ca9348f19b25363307006df8a75b0147068
Jawhar-Hakim <jawharhakim2016@gmail.com>
2026-09-07 00:54:58 +0100

    first commit

---
+++ CPU.h
@@ -0,0 +1,21 @@
+#ifndef CPU
+#define CPU
+#include <stdint.h>
+
+
+typedef struct
+{
+    uint8_t A;
+    uint8_t F;
+    uint8_t B;
+    uint8_t C;
+
+    uint8_t D;
+    uint8_t E;
+    uint8_t H;
+    uint8_t L;
+
+    uint16_t PC;
+    uint16_t SP;
+}CPU;
+#endif // CPU
---
+++ main.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdint.h>
+
+struct CPU
+{
+    uint8_t A;
+    uint8_t F;
+    uint8_t B;
+    uint8_t C;
+
+    uint8_t D;
+    uint8_t E;
+    uint8_t H;
+    uint8_t L;
+
+    uint16_t PC;
+    uint16_t SP;
+};
+
+uint8_t fetch(struct CPU *cpu, uint8_t memory[])
+{
+    uint8_t value = memory[cpu->PC];
+    cpu->PC++;
+    return value;
+}
+
+void ld_a_n(struct CPU *cpu, uint8_t memory[])
+{
+    cpu->A = fetch(cpu, memory);
+}
+
+void ld_b_n(struct CPU *cpu, uint8_t memory[])
+{
+    cpu->B = fetch(cpu,memory);
+}
+
+void inc_a(struct CPU *cpu)
+{
+    cpu->A++;
+}
+
+uint16_t get_bc(struct CPU *cpu)
+{
+    return (cpu->B << 8) | cpu->C;
+}
+
+void set_bc(struct CPU *cpu, uint16_t value)
+{
+    cpu->B = value >> 8;
+    cpu->C = value;
+}
+
+int main(void)
+{
+    struct CPU cpu;
+
+    uint8_t memory[65536];
+
+    cpu.A = 42;
+    cpu.PC = 0x0150;
+    uint8_t program[] = {
+        0x3E, 0x42,
+        0x3C,
+        0x06, 0x99
+    };
+
+
+    for (int i = 0; i < sizeof(program); i++)
+    {
+        memory[0x0150 + i] = program[i];
+    }
+
+
+
+    printf("PC     = 0x%04X\n", cpu.PC);
+
+
+    while(cpu.PC!=0x0155){
+         uint8_t opcode = fetch(&cpu, memory);
+         printf("Opcode = 0x%02X\n", opcode);
+         switch (opcode)
+        {
+            case 0x00:
+                break;
+
+            case 0x3E:
+                ld_a_n(&cpu,memory);
+                printf("A      = 0x%02X\n", cpu.A);
+                printf("PC     = 0x%04X\n", cpu.PC);
+                break;
+
+            case 0x3C:
+                inc_a(&cpu);
+                printf("A      = 0x%02X\n", cpu.A);
+                printf("PC     = 0x%04X\n", cpu.PC);
+                break;
+            case 0x06:
+                ld_b_n(&cpu,memory);
+                printf("B      = 0x%02X\n", cpu.B);
+                printf("PC     = 0x%04X\n", cpu.PC);
+                break;
+            default:
+                printf("Unknown opcode: 0x%02X\n", opcode);
+                break;
+        }
+    }
+    printf("-----------------\n");
+    printf("A      = 0x%02X\n", cpu.A);
+    printf("B      = 0x%02X\n", cpu.B);
+    printf("PC     = 0x%04X\n", cpu.PC);
+
+
+
+    return 0;
+}
