48b148fc6b2e33ef353c4f998e15fd5c3c0bbaef
Jawhar-Hakim <jawharhakim2016@gmail.com>
2026-09-18 19:12:03 +0100

    18/09/2026

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 struct CPU
 {
     uint8_t A;
-    uint8_t F;
+    uint8_t F;// z n h c 0000
     uint8_t B;
     uint8_t C;
 
@@ -36,7 +36,22 @@ void ld_b_n(struct CPU *cpu, uint8_t memory[])
 
 void inc_a(struct CPU *cpu)
 {
+    uint8_t old_a = cpu->A;
     cpu->A++;
+    // z flag
+    if(cpu->A == 0x00){
+        cpu->F |= 0x80;
+    } else{
+        cpu->F &= ~0x80; //same as cpu->F |= 0x70
+    }
+    // h flag
+    if ((old_a & 0x0F) == 0x0F){
+        cpu->F |= 0x20;
+    } else {
+        cpu->F &= ~0x20;
+    }
+    //n flag
+    cpu->F &= ~0x40;
 }
 
 uint16_t get_bc(struct CPU *cpu)
@@ -58,9 +73,41 @@ void ld_bc_nn(struct CPU *cpu, uint8_t memory[])
     set_bc(cpu, ((uint16_t)high << 8) | low);
 }
 
+void cp_a_n(struct CPU *cpu, uint8_t memory[])
+{
+    uint8_t n = fetch(cpu, memory);
+    uint8_t lower_a = cpu->A & 0x0F;
+    uint8_t lower_n = n & 0x0F;
+    if (cpu->A == n){
+        cpu->F |= 0x80;
+    } else {
+        cpu->F &= ~0x80;
+    }
+    cpu->F |= 0x40;
+    if(lower_a<lower_n){
+        cpu->F |= 0x20;
+    } else {
+        cpu->F &= ~0x20;
+    }
+    if(cpu->A<n){
+        cpu->F |= 0x10;
+    } else {
+        cpu->F &= ~0x10;
+    }
+}
+
+void jr_z_r8(struct CPU *cpu, uint8_t memory[])
+{
+    int8_t offset = (int8_t) (fetch(cpu,memory));
+    if(cpu->F & 0x80){
+        cpu->PC += offset;
+    }
+
+}
+
 int main(void)
 {
-    struct CPU cpu;
+    struct CPU cpu = {0};
 
     uint8_t memory[65536];
 
@@ -69,7 +116,10 @@ int main(void)
     uint8_t program[] = {
         0x01, 0x34, 0x12,  // LD BC, 0x1234
         0x3E, 0x42,        // LD A, 0x42
-        0x3C               // INC A
+        //0x3C,              // INC A
+        0xFE, 0x42,        // CP A, n
+        0x28, 0x02,        // JR Z, r8
+        0x06, 0x99         //LD B, n
     };
 
     for (int i = 0; i < sizeof(program); i++)
@@ -79,7 +129,7 @@ int main(void)
 
     printf("PC     = 0x%04X\n", cpu.PC);
 
-    while(cpu.PC!=0x0156){
+    while(cpu.PC!=0x0160){
          uint8_t opcode = fetch(&cpu, memory);
          printf("Opcode = 0x%02X\n", opcode);
          switch (opcode)
@@ -88,7 +138,7 @@ int main(void)
                 break;
 
             case 0x01:
-                ld_bc_nn(&cpu,memory); //incompatible type
+                ld_bc_nn(&cpu,memory);
                 break;
 
             case 0x3E:
@@ -102,16 +152,28 @@ int main(void)
                 printf("A      = 0x%02X\n", cpu.A);
                 printf("PC     = 0x%04X\n", cpu.PC);
                 break;
+            case 0xFE:
+                cp_a_n(&cpu,memory);
+                printf("A = 0x%02X, F = 0x%02X, PC = 0x%04X\n",
+                    cpu.A, cpu.F, cpu.PC);
+                break;
+            case 0x28:
+                jr_z_r8(&cpu, memory);
+                printf("After JR: PC = 0x%04X\n", cpu.PC);
+                break;
+            case 0x06:
+                ld_b_n(&cpu,memory);
+                break;
             default:
                 printf("Unknown opcode: 0x%02X\n", opcode);
                 break;
         }
     }
     printf("-----------------\n");
-    printf("A      = 0x%02X\n", cpu.A);
-    printf("B      = 0x%02X\n", cpu.B);
-    printf("BC     = 0x%04X\n", get_bc(&cpu)); //incompatible type
-    printf("PC     = 0x%04X\n", cpu.PC);
+    printf("A  = 0x%02X\n", cpu.A);
+    printf("B  = 0x%02X\n", cpu.B);
+    printf("F  = 0x%02X\n", cpu.F);
+    printf("PC = 0x%04X\n", cpu.PC);
 
     return 0;
 }
